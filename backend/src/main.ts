76f659e882fd03bfc29a2300cee33fb7dc0e7e15
Carlos Fernandes <carferdas@gmail.com>
2022-12-01 22:51:27 -0300

    init

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove fields without include in dto
+      forbidNonWhitelisted: true, // return bad request if have additinal fields no expected
+    }),
+  );
+
+  await app.listen(process.env.PORT);
+  new Logger('Bootstrap').log(`App running on port ${process.env.PORT}`);
+}
+bootstrap();
